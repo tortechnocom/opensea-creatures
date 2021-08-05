51f8386c24ff65a6bd329a21ab5112323b1b9ae3
Torrotto <tortechnocom@gmail.com>
2021-08-05 23:34:40 +0700

    First impression

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,10 +14,10 @@ const setupCreatureAccessories = require("../lib/setupCreatureAccessories.js");
 
 // If you want to hardcode what deploys, comment out process.env.X and use
 // true/false;
-const DEPLOY_ALL = process.env.DEPLOY_ALL;
-const DEPLOY_ACCESSORIES_SALE = process.env.DEPLOY_ACCESSORIES_SALE || DEPLOY_ALL;
-const DEPLOY_ACCESSORIES = process.env.DEPLOY_ACCESSORIES || DEPLOY_ACCESSORIES_SALE || DEPLOY_ALL;
-const DEPLOY_CREATURES_SALE = process.env.DEPLOY_CREATURES_SALE || DEPLOY_ALL;
+const DEPLOY_ALL = true;
+const DEPLOY_ACCESSORIES_SALE = true;
+const DEPLOY_ACCESSORIES = true;
+const DEPLOY_CREATURES_SALE = true;
 // Note that we will default to this unless DEPLOY_ACCESSORIES is set.
 // This is to keep the historical behavior of this migration.
 const DEPLOY_CREATURES = process.env.DEPLOY_CREATURES || DEPLOY_CREATURES_SALE || DEPLOY_ALL || (! DEPLOY_ACCESSORIES);
